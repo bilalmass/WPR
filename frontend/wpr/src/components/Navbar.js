9eb262d2760dff05de16f6c3b8f342979e36d3fc
Tony <21135037@student.hhs.nl>
2023-12-14 12:59:02 +0100

    navbar

--- frontend/wpr/src/components/Navbar.js
+++ frontend/wpr/src/components/Navbar.js
@@ -1,15 +1,23 @@
 ﻿// Navbar.jsx
 import React from 'react';
 import './componentstyling/navbar.css';  // Aangepaste import-paden
+import logo from './media/Logo Icon/icon_accessibility.png';
 
 const Navbar = () => {
     return (
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <div className="navbar-container">
+                <a href="#" className="navbar-logo">Stichting Accessibility</a>
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <ul className="navbar-links">
+                    <li><a href="#" className="navbar-link">Sectoren▼</a></li>
+                    <li><a href="#" className="navbar-link">Casussen▼</a></li>
+                    <li><a href="#" className="navbar-link">Toegankelijkheid▼</a></li>
+                    <li><a href="#" className="navbar-link">Actueel▼</a></li>
+                    <li><a href="#" className="navbar-link">Login▼</a></li>
+                </ul>
+            </div>
         </nav>
     );
 };

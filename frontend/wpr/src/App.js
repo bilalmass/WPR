276f3601184133e7b68df08e71f5514e9a297889
Tony <21135037@student.hhs.nl>
2023-12-14 11:51:06 +0100

    Basic pagina

--- frontend/wpr/src/App.js
+++ frontend/wpr/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// App.jsx
+import React from 'react';
+import logo from './components/media/Logo Icon/icon_accessibility.png';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './components/componentstyling/mainpage.css';  // Aangepaste import-paden
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="main-page">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <div className="main-title">
+                        Welkom bij Stichting Accessibility
+                    </div>
+                    <div className="sub-title">
+                        Een inclusieve samenleving waarin iedereen gelijkwaardig participeert.
+                    </div>
+                    <a className="btn" href="#lees-meer">
+                        Lees meer
+                    </a>
+                </header>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/wpr/src/components/Navbar.js
@@ -0,0 +1,17 @@
+﻿// Navbar.jsx
+import React from 'react';
+import './componentstyling/navbar.css';  // Aangepaste import-paden
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;

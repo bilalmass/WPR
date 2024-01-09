2a5a83a8f2fc1007604b801583fd25a96e838d8e
bilalmass <76113339+bilalmass@users.noreply.github.com>
2024-01-09 15:08:42 +0100

    ci/cd foutmelding

--- frontend/wpr/src/components/Registerpage.js
+++ frontend/wpr/src/components/Registerpage.js
@@ -1,7 +1,7 @@
 ﻿/* Register.js */
 import React, { useState } from 'react';
 import './componentstyling/registerpage.css';
-import axios from 'axios';
+//import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({

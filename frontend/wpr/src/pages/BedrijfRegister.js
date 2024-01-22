b14813649c43db41aaa7ce8d79ef34a0fd5e0ef0
BilalElhassani <dakar_ali9@hotmail.com>
2024-01-22 11:38:41 +0100

    ,,

--- frontend/wpr/src/pages/BedrijfRegister.js
+++ frontend/wpr/src/pages/BedrijfRegister.js
@@ -1,16 +1,14 @@
-﻿import Navbar from '../components/Navbar'
-import BedrijfRegister from '../components/BedrijfRegister'
-import Footer from '../components/Footer'
-
-
+﻿import React from 'react';
+import Navbar from '../components/Navbar';
+import BedrijfRegister from '../components/BedrijfRegister';
+import Footer from '../components/Footer';
 
 export default function Register() {
-    return(
-
+    return (
         <>
-            <Navbar/>
-            <BedrijfRegister/>
-            <Footer/>
+            <Navbar />
+            <BedrijfRegister />
+            <Footer />
         </>
-    )
+    );
 }
